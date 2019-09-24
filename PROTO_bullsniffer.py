0032bcea428412705e72ba4e35b62d552914dee3
Werdeles Marcio de C. Soares <36682515+werdelesmarcio@users.noreply.github.com>
2019-09-24 17:43:01 -0300

    Update PROTO_bullsniffer.py

--- PROTO_bullsniffer.py
+++ PROTO_bullsniffer.py
@@ -3,29 +3,64 @@
 import socket
 import os
 
-host = "192.168.56.1"
+#Packet sniffer in python for Linux
+#Sniffs only incoming TCP packet
 
-# criando um raw socket e um bind para uma interface publica
-if os.name == "nt":
-    socket_protocol = socket.IPPROTO_IP
-else:
-    socket_protocol = socket.IPPROTO_ICMP
+import socket, sys
+from struct import *
 
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+#create an INET, STREAMing socket
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+except socket.error , msg:
+	print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
 
-sniffer.bind((host, 0))
-
-# queremos capturar o cabeçalho do ip
-sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
-# se estivermos no Windows, nós temos que enviar um IOCTL
-# para setar o modo promíscuo
-if os.name == "nt":
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-# lendo o pacote
-print sniffer.recvfrom(65565)
-
-# se estivermos no Windows, temos que desligar o modo promiscuo
-if os.name == "nt":
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+# receive a packet
+while True:
+	packet = s.recvfrom(65565)
+	
+	#packet string from tuple
+	packet = packet[0]
+	
+	#take first 20 characters for the ip header
+	ip_header = packet[0:20]
+	
+	#now unpack them :)
+	iph = unpack('!BBHHHBBH4s4s' , ip_header)
+	
+	version_ihl = iph[0]
+	version = version_ihl &gt;&gt; 4
+	ihl = version_ihl &amp; 0xF
+	
+	iph_length = ihl * 4
+	
+	ttl = iph[5]
+	protocol = iph[6]
+	s_addr = socket.inet_ntoa(iph[8]);
+	d_addr = socket.inet_ntoa(iph[9]);
+	
+	print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
+	
+	tcp_header = packet[iph_length:iph_length+20]
+	
+	#now unpack them :)
+	tcph = unpack('!HHLLBBHHH' , tcp_header)
+	
+	source_port = tcph[0]
+	dest_port = tcph[1]
+	sequence = tcph[2]
+	acknowledgement = tcph[3]
+	doff_reserved = tcph[4]
+	tcph_length = doff_reserved &gt;&gt; 4
+	
+	print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
+	
+	h_size = iph_length + tcph_length * 4
+	data_size = len(packet) - h_size
+	
+	#get data from the packet
+	data = packet[h_size:]
+	
+	print 'Data : ' + data
+	print
