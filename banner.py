78a9c3d23c14510d22bb5109196d98f4178524ed
Werdeles Marcio de C. Soares <36682515+werdelesmarcio@users.noreply.github.com>
2019-09-25 17:38:40 -0300

    Update banner.py

--- banner.py
+++ banner.py
@@ -5,17 +5,17 @@
 import sys
 
 def banner():
-	print("     ___________                               ")
-	print("    / ========= \                              ")
-	print("   /_____________\                             ")
-	print("  | _____________ |                            ")
-	print("  | | -         | |                            ")
-	print("  | |           | |                            ")
-	print("  | |___________| |__________________________  ")
-	print("  |_=_____________|       BULLSNIFFER        ) ")
-	print("  / ::::::::::::: \       VERSION 1.0       /  ")
-	print(" / ::::::::::::::: \      BY: GHOST     =D-'   ")
-	print("(___________________)                          ")
+	print("       ___________                                    ")    
+    	print("      / ========= \                                   ")
+   	print("     /_____________\                                  ")
+  	print("    | _____________ |                                 ")
+  	print("    | | -         | |                                 ")
+  	print("    | |           | |                                 ")
+  	print("    | |___________| |________________________________ ") 
+  	print("    |_=_____________|          BULLSNIFFER           )") 
+  	print("    / ::::::::::::: \          VERSION 1.0          / ") 
+ 	print("   / ::::::::::::::: \          BY: GHOST       =D-'  ") 
+	print("  (___________________)    Vault Cyber Security       ")
 	print
 
 	return banner
