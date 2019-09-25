0dc2d2a95b64ee36f4fe484e140514ccb699c5f4
Werdeles Marcio de C. Soares <36682515+werdelesmarcio@users.noreply.github.com>
2019-09-25 16:09:02 -0300

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+import sys
+
+def banner():
+	print("     ___________                               ")
+	print("    / ========= \                              ")
+	print("   /_____________\                             ")
+	print("  | _____________ |                            ")
+	print("  | | -         | |                            ")
+	print("  | |           | |                            ")
+	print("  | |___________| |__________________________  ")
+	print("  |_=_____________|       BULLSNIFFER        ) ")
+	print("  / ::::::::::::: \       VERSION 1.0       /  ")
+	print(" / ::::::::::::::: \      BY: GHOST     =D-'   ")
+	print("(___________________)                          ")
+	print
+
+	return banner
